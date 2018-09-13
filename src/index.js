0f54c8173a85999b8d97d49bdd4bdc97ed72127e
sqwezzy <maks.subbotko46@gmail.com>
2018-09-13 21:09:09 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount (preferences = []) {
+  return Math.floor(preferences.filter((item, i) =>
+        preferences[preferences[preferences[i] - 1] - 1] === i + 1).length / 3);
 };
